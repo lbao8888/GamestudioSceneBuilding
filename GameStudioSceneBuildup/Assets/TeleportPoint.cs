c215c9f3cc1a61efa917def122ef2206904d31ba
lbao8888 <lbao8@pratt.edu>
2026-04-22 00:57:39 -0400

    143414

---
+++ GameStudioSceneBuildup/Assets/TeleportPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TeleportPoint : MonoBehaviour
+{
+    [Header("传送目标位置")]
+    public Transform targetPoint;
+
+    [Header("玩家标签")]
+    public string playerTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag) && targetPoint != null)
+        {
+            other.transform.position = targetPoint.position;
+        }
+    }
+}
